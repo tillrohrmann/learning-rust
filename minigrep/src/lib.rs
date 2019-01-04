7ff662ce09c7816731f2d352df6b27c177994e6a
Till Rohrmann <trohrmann@apache.org>
2019-01-04 09:48:21 +0100

    Finish search logic for minigrep

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -4,9 +4,22 @@ use std::error::Error;
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let file_content = fs::read_to_string(config.filename)?;
 
-    println!("File content:\n{}", file_content);
+    for line in search(&config.query, &file_content) {
+        println!("{}", line);
+    }
 
     Ok(())
+}
+
+fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+    for line in content.lines() {
+        if line.contains(query) {
+          result.push(line);
+        }
+    }
+
+    result
 }
 
 pub struct Config {
@@ -27,5 +40,23 @@ impl Config {
             query,
             filename
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_search() {
+        let query = "duct";
+        let content = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, content));
     }
 }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -12,9 +12,6 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for: {}.", config.query);
-    println!("In file: {}.", config.filename);
-
     if let Err(e) = minigrep::run(config) {
         println!("Error occurred: {}", e);
         process::exit(1);
